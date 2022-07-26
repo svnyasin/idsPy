ffca904b859a3e63c329a3b5ac68c6efbef99011
Yasin Seven <67184421+svnyasin@users.noreply.github.com>
2022-07-26 14:03:12 +0300

    private

--- alert/admin.py
+++
@@ -1,7 +0,0 @@
-import imp
-from django.contrib import admin
-from .models import CapturedPacket, ArchivedPacket
-# Register your models here.
-
-admin.site.register(CapturedPacket)
-admin.site.register(ArchivedPacket)
